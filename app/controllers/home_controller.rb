2e93547649d8cafb0ea8025749894844af2650bb
manoel bertin <manoeltirj@gmail.com>
2023-07-15 16:49:39 -0300

    aqui eu iterei com meu array devs pra imprimir linha por linha nome e linguagem de prog.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
+    #@name = ' Alibaba'
+
+    @devs = [
+      {name: 'xiita', dev: 'ruby'},
+      {name: 'cacau', dev: 'rusty'},
+      {name: 'betinho', dev: 'python'}
+    ]
     #render html: '<h1> Olá Mundo! <h1>'.html_safe
   end
 end
