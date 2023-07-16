d2464c12b7f03664787f145d200673eccefe4fb6
manoel bertin <manoeltirj@gmail.com>
2023-07-16 14:57:15 -0300

    aqui o Rails já reconhece em uma linha o padrão RestFull.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   get '/home', controller: 'home', action: 'index'
 
-  get '/vehicles', controller: 'vehicles', action: 'index'
+  # get '/vehicles', controller: 'vehicles', action: 'index'
 
-  get '/vehicles/new', controller: 'vehicles', action: 'new'
+  # get '/vehicles/new', controller: 'vehicles', action: 'new'
 
-  post '/vehicles', controller: 'vehicles', action: 'create'
+  # post '/vehicles', controller: 'vehicles', action: 'create'
 
-  get '/vehicles/:id', controller: 'vehicles', action: 'show'
+  # get '/vehicles/:id', controller: 'vehicles', action: 'show'
 
-  get '/vehicles/:id/edit', controller: 'vehicles', action: 'edit'
+  # get '/vehicles/:id/edit', controller: 'vehicles', action: 'edit'
 
-  put '/vehicles/:id', controller: 'vehicles', action: 'update'
-  patch '/vehicles/:id', controller: 'vehicles', action: 'update'
-  
-  delete '/vehicles/:id', controller: 'vehicles', action: 'destroy'
-  #resources :vehicles, only: [:index]
+  # put '/vehicles/:id', controller: 'vehicles', action: 'update'
+  # patch '/vehicles/:id', controller: 'vehicles', action: 'update'
+
+  # delete '/vehicles/:id', controller: 'vehicles', action: 'destroy'
+  resources :vehicles, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
