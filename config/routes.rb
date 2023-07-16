831509008a6de8b5070507aeba05301ee3e78398
manoel bertin <manoeltirj@gmail.com>
2023-07-16 14:45:44 -0300

    aqui nós temos 4 AÇOES básicas CRUD ==> que geram 7 rotas.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   get '/home', controller: 'home', action: 'index'
+
+  get '/vehicles', controller: 'vehicles', action: 'index'
+
+  get '/vehicles/new', controller: 'vehicles', action: 'new'
+
+  post '/vehicles', controller: 'vehicles', action: 'create'
+
+  get '/vehicles/:id', controller: 'vehicles', action: 'show'
+
+  get '/vehicles/:id/edit', controller: 'vehicles', action: 'edit'
+
+  put '/vehicles/:id', controller: 'vehicles', action: 'update'
+  patch '/vehicles/:id', controller: 'vehicles', action: 'update'
+  
+  delete '/vehicles/:id', controller: 'vehicles', action: 'destroy'
+  #resources :vehicles, only: [:index]
 end
