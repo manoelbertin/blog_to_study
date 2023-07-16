e423e120454c27d595fa4a5853dd49100126dff0
manoel bertin <manoeltirj@gmail.com>
2023-07-16 07:57:25 -0300

    aqui fiz a criação do array com 'new' e perstisti com o 'save' e logo após fiz outra criação com o 'create' Para validar usa-se a primeira parte 'new', 'validate' e 'save'.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_15_183657) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_16_004405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "kind"
+    t.string "plate"
   end
 
 end
